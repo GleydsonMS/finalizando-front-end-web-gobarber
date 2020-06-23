409ff012e726b43d393ebec5bf0b38fadf679b87
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2020-06-23 20:12:45 -0300

    Criando Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { FiPower } from 'react-icons/fi';
+import { Container, Header, HeaderContent, Profile } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { singOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img
+              src="https://avatars1.githubusercontent.com/u/8749369?s=460&u=0afa4150ab4e64a0626ac15c6acd596748e93cf0&v=4"
+              alt="Gleydson Miranda {/* user.name */}"
+            />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>Gleydson Miranda {/* user.name */}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={singOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;
