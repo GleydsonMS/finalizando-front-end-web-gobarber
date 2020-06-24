8e656e61f438eca1e6de9e6a466324771c58b698
Gleydson Miranda Souza <gleydson.m.s.1993@gmail.com>
2020-06-23 21:24:08 -0300

    Listagem de agendamentos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FiPower, FiClock } from 'react-icons/fi';
 import {
@@ -9,6 +9,8 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 
@@ -16,6 +18,8 @@ import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { singOut, user } = useAuth();
 
   return (
@@ -65,6 +69,62 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/8749369?s=460&u=0afa4150ab4e64a0626ac15c6acd596748e93cf0&v=4"
+                  alt="Gleydson Miranda"
+                />
+
+                <strong>Gleydson Miranda</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/8749369?s=460&u=0afa4150ab4e64a0626ac15c6acd596748e93cf0&v=4"
+                  alt="Gleydson Miranda"
+                />
+
+                <strong>Gleydson Miranda</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/8749369?s=460&u=0afa4150ab4e64a0626ac15c6acd596748e93cf0&v=4"
+                  alt="Gleydson Miranda"
+                />
+
+                <strong>Gleydson Miranda</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
